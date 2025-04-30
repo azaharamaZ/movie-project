c477481c6b198af40f54c9a1a58b9595dfb1c8a9
Azahara <azahara22lopez@gmail.com>
2025-04-30 20:56:36 +0200

    [EDIT] minor fix

--- apps/movie-project/src/app/layouts/navbar/navbar.component.ts
+++ apps/movie-project/src/app/layouts/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink, RouterLinkActive, SearchComponent],
+  imports: [RouterLink, RouterLinkActive, SearchComponent],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
